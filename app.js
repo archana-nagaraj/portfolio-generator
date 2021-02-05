28539390f77e21dfbefb0b91e504b63c0f49213c
Archana Nagaraja <johndoe@example.com>
2021-02-05 09:33:19 -0800

    code cleanup

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const username = profileDataArgs[0];
-// const github = profileDataArgs[1];
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const [username, github] = profileDataArgs; // ES6 feature assignemnt destructuring
+const profileDataArgs = process.argv.slice(2);
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+console.log(profileDataArgs);
 
-//   console.log('================');
+const [name, github] = profileDataArgs;
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+console.log(name, github);
 
-// printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github);
 
-//const generatePage = () => 'name: Jane, Github: janehub';
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-
-// const generatePage = (userName, githubName) => {
-//     return`
-//     Name: ${userName} 
-//     Github: ${githubName}
-//     `;
-// };
-
-
-
-
-  fs.writeFile('index.html', generatePage(username, github), err => 
-  {
-    if (err) throw err;
-    console.log("Portfolio complete! Check out index.html to see the output!'");
-  })
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
